fa9bb34a898af3faf424ec264c6b0893d2dda5bd
Christopher Doris <github.com/cjdoris>
2022-03-31 17:52:43 +0100

    dev

--- src/ShadeYourData.jl
+++ src/ShadeYourData.jl
@@ -1,8 +1,7 @@
 module ShadeYourData
 
-export datashade!
-
-using AbstractPlotting, StatsBase, Requires
+import Base.Threads: @threads
+import Makie: Makie, Heatmap, Image, Axis, Theme, FRect3D, heatmap!, image!, lift, @recipe, default_theme, (..), observe_changes
 
 struct Canvas{T}
     xmin :: T
@@ -16,6 +15,7 @@ end
 Base.size(c::Canvas) = (c.xsize, c.ysize)
 xlims(c::Canvas) = (c.xmin, c.xmax)
 ylims(c::Canvas) = (c.ymin, c.ymax)
+Base.:(==)(a::Canvas, b::Canvas) = size(a)==size(b) && xlims(a)==xlims(b) && ylims(a)==ylims(b)
 
 abstract type AggOp end
 
@@ -23,53 +23,95 @@ update(a::AggOp, x, args...) = merge(a, x, embed(a, args...))
 
 struct AggCount{T} <: AggOp end
 AggCount() = AggCount{Int}()
-null(a::AggCount{T}) where {T} = zero(T)
-embed(a::AggCount{T}) where {T} = oneunit(T)
-merge(a::AggCount{T}, x::T, y::T) where {T} = x + y
-value(a::AggCount{T}, x::T) where {T} = x
+null(::AggCount{T}) where {T} = zero(T)
+embed(::AggCount{T}) where {T} = oneunit(T)
+merge(::AggCount{T}, x::T, y::T) where {T} = x + y
+value(::AggCount{T}, x::T) where {T} = x
 
-struct AggAny{T} <: AggOp end
-AggAny() = AggAny{Bool}()
-null(a::AggAny{T}) where {T} = zero(T)
-embed(a::AggAny{T}) where {T} = oneunit(T)
-merge(a::AggAny{T}, x::T, y::T) where {T} = max(x, y)
-value(a::AggAny{T}, x::T) where {T} = x
+struct AggAny <: AggOp end
+null(::AggAny) = false
+embed(::AggAny) = true
+merge(::AggAny, x::Bool, y::Bool) = x | y
+value(::AggAny, x::Bool) = x
 
 struct AggSum{T} <: AggOp end
 AggSum() = AggSum{Float64}()
-null(a::AggSum{T}) where {T} = zero(T)
-embed(a::AggSum{T}, x) where {T} = convert(T, x)
-merge(a::AggSum{T}, x::T, y::T) where {T} = x + y
-value(a::AggSum{T}, x::T) where {T} = x
+null(::AggSum{T}) where {T} = zero(T)
+embed(::AggSum{T}, x) where {T} = convert(T, x)
+merge(::AggSum{T}, x::T, y::T) where {T} = x + y
+value(::AggSum{T}, x::T) where {T} = x
 
-struct AggMean{T,N} <: AggOp end
-AggMean{T}() where {T} = AggMean{T,Int}()
+struct AggMean{T} <: AggOp end
 AggMean() = AggMean{Float64}()
-null(a::AggMean{T,N}) where {T,N} = zero(T), zero(N)
-embed(a::AggMean{T,N}, x) where {T,N} = convert(T,x), oneunit(N)
-merge(a::AggMean{T,N}, x::Tuple{T,N}, y::Tuple{T,N}) where {T,N} = x[1]+y[1], x[2]+y[2]
-value(a::AggMean{T,N}, x::Tuple{T,N}) where {T,N} = float(x[1]) / float(x[2])
+null(::AggMean{T}) where {T} = (zero(T), zero(T))
+embed(::AggMean{T}, x) where {T} = (convert(T,x), oneunit(T))
+merge(::AggMean{T}, x::Tuple{T,T}, y::Tuple{T,T}) where {T} = (x[1]+y[1], x[2]+y[2])
+value(::AggMean{T}, x::Tuple{T,T}) where {T} = float(x[1]) / float(x[2])
 
-function aggregate(c::Canvas, xs, ys, zs...; op::AggOp=AggCount())
+abstract type AggMethod end
+
+struct AggSerial <: AggMethod end
+struct AggThreads <: AggMethod end
+
+aggregate(c::Canvas, xs, ys, zs...; op::AggOp=AggCount(), method::AggMethod=AggSerial()) =
+    _aggregate(c, op, method, xs, ys, zs...)
+
+function _aggregate(c::Canvas, op::AggOp, method::AggSerial, xs, ys, zs...)
     xmin, xmax = xlims(c)
     ymin, ymax = ylims(c)
     xsize, ysize = size(c)
     xmax > xmin || error("require xmax > xmin")
     ymax > ymin || error("require ymax > ymin")
-    xscale = 1 / (xmax - xmin)
-    yscale = 1 / (ymax - ymin)
+    xscale = xsize / (xmax - xmin)
+    yscale = ysize / (ymax - ymin)
     out = fill(null(op), xsize, ysize)
-    for row in zip(xs, ys, zs...)
-        x = row[1]
-        y = row[2]
-        z = row[3:end]
+    @inbounds for idx in eachindex(xs, ys, zs...)
+        x = xs[idx]
+        y = ys[idx]
+        z = map(z->z[idx], zs)
         xmin ≤ x ≤ xmax || continue
         ymin ≤ y ≤ ymax || continue
-        i = clamp(1+floor(Int, xsize*xscale*(x-xmin)), 1, xsize)
-        j = clamp(1+floor(Int, ysize*yscale*(y-ymin)), 1, ysize)
-        @inbounds out[i,j] = update(op, out[i,j], z...)
+        i = clamp(1+floor(Int, xscale*(x-xmin)), 1, xsize)
+        j = clamp(1+floor(Int, yscale*(y-ymin)), 1, ysize)
+        out[i,j] = update(op, out[i,j], z...)
+        nothing
     end
     map(x->value(op, x), out)
+end
+
+function _aggregate(c::Canvas, op::AggOp, method::AggThreads, xs, ys, zs...)
+    xmin, xmax = xlims(c)
+    ymin, ymax = ylims(c)
+    xsize, ysize = size(c)
+    xmax > xmin || error("require xmax > xmin")
+    ymax > ymin || error("require ymax > ymin")
+    xscale = xsize / (xmax - xmin)
+    yscale = ysize / (ymax - ymin)
+    # each thread reduces some of the data separately
+    out = fill(null(op), nthreads(), xsize, ysize)
+    @threads for idx in eachindex(xs, ys, zs...)
+        t = threadid()
+        x = @inbounds xs[idx]
+        y = @inbounds ys[idx]
+        z = map(z->@inbounds(z[idx]), zs)
+        xmin ≤ x ≤ xmax || continue
+        ymin ≤ y ≤ ymax || continue
+        i = clamp(1+floor(Int, xscale*(x-xmin)), 1, xsize)
+        j = clamp(1+floor(Int, yscale*(y-ymin)), 1, ysize)
+        @inbounds out[t,i,j] = update(op, out[t,i,j], z...)
+    end
+    # reduce along the thread dimension
+    out2 = fill(null(op), xsize, ysize)
+    for j in 1:ysize
+        for i in 1:xsize
+            @inbounds val = out[1,i,j]
+            for t in 2:nthreads()
+                @inbounds val = merge(op, val, out[t,i,j])
+            end
+            @inbounds out2[i,j] = val
+        end
+    end
+    map(x->value(op, x), out2)
 end
 
 const DEFAULT_SPREAD_MASKS = Matrix{Bool}[
@@ -114,37 +156,14 @@ function autospread(img::AbstractMatrix; masks=DEFAULT_SPREAD_MASKS, rmax=length
         s = count(x->!iszero(x), mask)
         newout = spread(img, mask; opts...)
         n = count(x->!iszero(x), newout)
-        n ≥ thresh * s * n₀ || return out
-        out = newout
+        n < thresh * s * n₀ && break
+        # out = newout
+        # linearly interpolate between out and newout depending on where n is in the
+        # interval [thresh * s * n₀, s * n₀]
+        p = (n / (s * n₀) - thresh) / (1 - thresh)
+        out = @. p * newout + (1 - p) * out
     end
     return out
-end
-
-function autospread_old(img::AbstractMatrix, rmax::Integer=5, op=max; thresh=0.2)
-    outmax = spread(img, rmax, op)
-    nummax = count(x->!iszero(x), outmax)
-    for r in 0:rmax-1
-        out = spread(img, r, op)
-        num = count(x->!iszero(x), out)
-        if num ≥ thresh * nummax
-            return out
-        end
-    end
-    return outmax
-end
-
-canvas_node(c::Canvas) = Node{Canvas}(c)
-canvas_node(c::Node{<:Canvas}) = c
-
-function __init__()
-    @require MakieLayout="5a521ce4-ebb9-4793-b5b7-b334dfe8393c" begin
-        canvas_node(ax::MakieLayout.LAxis) = lift(ax.limits, ax.scene.px_area) do lims, pxarea
-            xsize, ysize = widths(pxarea)
-            xmin, ymin = minimum(lims)
-            xmax, ymax = maximum(lims)
-            Canvas(xmin, xmax, xsize, ymin, ymax, ysize)
-        end
-    end    
 end
 
 """
@@ -160,23 +179,28 @@ If `colorize` is given, it is a map which converts the post-processed matrix int
 
 All other options are passed on to the plotting command.
 """
-function datashade!(scene::AbstractScene, x, y, z...; colorize=nothing, opts...)
-    c = canvas_node(scene)
+function datashade!(ax::Axis, x, y, z...; colorize=nothing, opts...)
+    c = lift(ax.finallimits, ax.scene.px_area) do lims, pxarea
+        xsize, ysize = Makie.widths(pxarea)
+        xmin, ymin = minimum(lims)
+        xmax, ymax = maximum(lims)
+        Canvas(xmin, xmax, xsize, ymin, ymax, ysize)
+    end |> observe_changes
     if isnothing(colorize)
-        datashadeheatmap!(scene, c, x, y, z...; opts...)
+        datashadeheatmap!(ax, c, x, y, z...; opts...)
     else
-        datashadeimage!(scene, c, x, y, x...; colorize=colorize, opts...)
+        datashadeimage!(ax, c, x, y, x...; colorize=colorize, opts...)
     end
 end
 
-@recipe(DataShadeHeatmap, canvas, x, y, z) do scene
+@recipe(DataShadeHeatmap, canvas, x, y) do scene
     th = default_theme(scene, Heatmap)
     th.agg = AggCount()
     th.post = identity
     th
 end
 
-@recipe(DataShadeImage, canvas, x, y, z) do scene
+@recipe(DataShadeImage, canvas, x, y) do scene
     th = default_theme(scene, Image)
     th.agg = AggCount()
     th.post = identity
@@ -184,8 +208,7 @@ end
     th
 end
 
-function AbstractPlotting.plot!(p::DataShadeHeatmap{<:Tuple{Canvas,Vararg}})
-    global LAST_PLOT = p
+function Makie.plot!(p::DataShadeHeatmap{<:Tuple{Canvas,Vararg}})
     xrange = lift(c -> c.xmin .. c.xmax, p.canvas)
     yrange = lift(c -> c.ymin .. c.ymax, p.canvas)
     pixels = lift(p.agg, p.post, p.converted...) do agg, post, args...
@@ -196,10 +219,10 @@ function AbstractPlotting.plot!(p::DataShadeHeatmap{<:Tuple{Canvas,Vararg}})
         th[k] = p[k]
     end
     heatmap!(p, xrange, yrange, pixels; th...)
+    p
 end
 
-function AbstractPlotting.plot!(p::DataShadeImage{<:Tuple{Canvas,Vararg}})
-    global LAST_PLOT = p
+function Makie.plot!(p::DataShadeImage{<:Tuple{Canvas,Vararg}})
     xrange = lift(c -> c.xmin .. c.xmax, p.canvas)
     yrange = lift(c -> c.ymin .. c.ymax, p.canvas)
     pixels = lift(p.agg, p.post, p.colorize, p.converted...) do agg, post, colorize, args...
@@ -212,10 +235,10 @@ function AbstractPlotting.plot!(p::DataShadeImage{<:Tuple{Canvas,Vararg}})
     image!(p, xrange, yrange, pixels; th...)
 end
 
-function AbstractPlotting.data_limits(p::Union{DataShadeHeatmap{<:Tuple{Canvas,Vararg}}, DataShadeImage{<:Tuple{Canvas, Vararg}}})
+function Makie.data_limits(p::Union{DataShadeHeatmap{<:Tuple{Canvas,Vararg}}, DataShadeImage{<:Tuple{Canvas, Vararg}}})
     xmin, xmax = extrema(p.x[])
     ymin, ymax = extrema(p.y[])
     FRect3D([xmin, ymin, 0], [xmax-xmin, ymax-ymin, 0])
 end
 
-end # module
+end
